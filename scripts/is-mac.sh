145d994dbb76d3ae7e1b41fa3590cf02b920fcd7
arielszekely <arielszekely@gmail.com>
2019-12-06 12:12:08 -0600

    Better is-mac checking

--- scripts/is-mac.sh
+++ scripts/is-mac.sh
@@ -3,7 +3,7 @@
 # Check if we're running on Mac
 uname="$(uname -s)"
 if [ $uname == 'Darwin' ]; then
-  exit 1
+  echo 1
 else
-  exit 0
+  echo 0
 fi
--- ssh/install_ssh.sh
+++ ssh/install_ssh.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo 'Linking ssh'
+rm ~/.ssh/ssh-servers
 rm ~/.ssh/config
-ln -s ~/.dotfiles/ssh/ssh-config/config ~/.ssh/config
+ln -s ~/.dotfiles/ssh/ssh-config/ssh-servers ~/.ssh/ssh-servers
+if [ "$(~/.dotfiles/scripts/is-mac.sh)" ]; then
+  ln -s ~/.dotfiles/ssh/ssh-config/mac-config ~/.ssh/config
+else
+  ln -s ~/.dotfiles/ssh/ssh-config/config ~/.ssh/config
+fi
