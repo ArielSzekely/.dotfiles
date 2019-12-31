641006b908729c93db8687533f0e67a03dcd5de3
arielszekely <arielszekely@gmail.com>
2019-12-31 11:22:24 -0600

    Fixed install script

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -9,6 +9,13 @@ if [ -d '$/.oh-my-zsh' ]; then
 fi
 
 rm ~/.zshrc
-rm -rf ~/.oh-my-zsh/custom
+rm -f ~/.oh-my-zsh/custom/completion.zsh
+rm -f ~/.oh-my-zsh/custom/navigation.zsh
+rm -f ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+rm -f ~/.oh-my-zsh/custom/themes/arielszekely.zsh-theme
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/zsh/.oh-my-zsh/custom ~/.oh-my-zsh/custom
+ln -s ~/.dotfiles/zsh/.oh-my-zsh/custom/completion.zsh ~/.oh-my-zsh/custom/completion.zsh
+ln -s ~/.dotfiles/zsh/.oh-my-zsh/custom/navigation.zsh ~/.oh-my-zsh/custom/navigation.zsh
+ln -s ~/.dotfiles/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s ~/.dotfiles/zsh/.oh-my-zsh/custom/themes/arielszekely.zsh-theme ~/.oh-my-zsh/custom/themes/arielszekely.zsh-theme
+
