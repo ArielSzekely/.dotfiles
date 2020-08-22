9a31fbf0ec99515b59f19e3c71998e6ad6313258
Ariel Szekely <arielszekely@gmail.com>
2020-08-22 17:31:47 -0400

    fixed zsh install script

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -5,7 +5,7 @@ echo 'Linking zsh'
 # Clone oh-my-zsh if it isn't installed already
 if [ ! -d '~/.oh-my-zsh' ]; then
   echo 'Installing .oh-my-zsh'
-  ./install_oh_my_zsh.sh
+  ./zsh/install_oh_my_zsh.sh
 fi
 
 rm ~/.zshrc
