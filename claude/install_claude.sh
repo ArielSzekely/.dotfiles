dc6e64ff2e4430e5f60856a47ce978330c16b1d1
Ariel Szekely <arielck@mit.edu>
2026-04-17 06:57:24 -0400

    Claude initial file

---
+++ claude/install_claude.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'Linking claude'
+mkdir -p ~/.claude
+ln -s ~/.dotfiles/claude/settings.json ~/.claude/settings.json
--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ echo 'Linking all'
 ./vim/install_vim.sh
 ./zsh/install_zsh.sh
 ./tmux/install_tmux.sh
+./claude/install_claude.sh
 ./ssh/install_ssh.sh
